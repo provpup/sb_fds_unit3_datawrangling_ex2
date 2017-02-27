9569e78f26b860fff9a6dd80a3e08588726c2925
Kenny Teng <fof.kenny.teng@gmail.com>
2017-02-26 22:44:41 -0800

    Add script and cleaned data

---
+++ clean_titanic.R
@@ -0,0 +1,50 @@
+# install.packages('dplyr')
+# install.packages('tidyr')
+# install.packages('readr')
+library(dplyr)
+library(tidyr)
+library(readr)
+
+data <- read_csv('./titanic_original.csv')
+data <- tbl_df(data)
+
+# Fill NA values with S for the embarked column
+clean_embarked <- function (embarked) {
+  if (is.na(embarked)) {
+    return('S')
+  } else {
+    return(embarked)
+  }
+}
+
+mean_age = mean(data$age, na.rm = TRUE)
+# Fill NA values with the mean age for the age column
+clean_age <- function (age) {
+  if (is.na(age)) {
+    return(mean_age)
+  } else {
+    return(age)
+  }
+}
+
+# Fill NA values with 'None' for the boat column
+clean_boat <- function (boat) {
+  if (is.na(boat)) {
+    return('None')
+  } else {
+    return(boat)
+  }
+}
+
+# Represent the presence of a cabin value as 1 and absence with 0
+clean_cabin <- function (cabin) {
+  return(as.numeric(!is.na(cabin)))
+}
+
+data <- data %>%
+  mutate(embarked = sapply(embarked, clean_embarked),
+         age = sapply(age, clean_age),
+         boat = sapply(boat, clean_boat),
+         has_cabin_number = sapply(cabin, clean_cabin))
+
+write_csv(data, './titanic_clean.csv')
